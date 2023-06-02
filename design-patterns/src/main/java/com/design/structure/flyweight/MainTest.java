0226f032dbf6a93ad65fcaf9768c04c57f823b4c
Ahir <pipicop@163.com>
2023-06-03 07:36:20 +0800

    享元模式

--- design-patterns/src/main/java/com/design/structure/flyweight/MainTest.java
+++ design-patterns/src/main/java/com/design/structure/flyweight/MainTest.java
@@ -1,5 +1,10 @@
 package com.design.structure.flyweight;
 
+import com.sun.media.jfxmedia.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.sun.corba.se.impl.activation.ServerMain.logError;
+
 /**
  * @Title: MainTest
  * @Description: TODO
@@ -7,8 +12,24 @@ package com.design.structure.flyweight;
  * @Date: 2023/5/21 09:11
  * @Version: 1.0
  */
+@Slf4j
 public class MainTest{
 	public static void main(String[] args) {
+		try {
+			flyweightPatternTest();
+
+		} catch (Exception e) {
+			logError(e);
+		}
+
+
+	}
+
+	private static void logError(Exception e) {
+		System.out.println(e.getMessage());
+	}
+
+	private static void flyweightPatternTest() {
 		AbstractWaitressFlyweight waitress = ZuDao.getWaitress("");
 		waitress.getService();
 
